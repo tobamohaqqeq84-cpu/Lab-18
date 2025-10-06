a38004eb2a09ef2c8de4c76a530bf027229293c3
tobam <tobamohaqqeq84@gmail.com>
2025-10-06 01:52:51 +0000

    new change

--- main.cpp
+++ main.cpp
@@ -6,23 +6,35 @@
 #include <string>
 using namespace std;
 
+// each node holds a rating and a comment.
 struct Node {
       float rating;
       string comment;
-      Node next;
+      Node* next; // pointer to next node in the list.
                 };
-
-void addAtHead (Node head, float rating, string comment);
-void addAtTail (Node head, float rating, string comment);
-void printlist (Node head);
+// function prototypes.
+void addAtHead (Node*& head, float rating, string comment);
+void addAtTail (Node*& head, float rating, string comment);
+void printlist (Node* head);
 
 int main(){
-    Node head = NULL;
+    Node* head = nullptr;
     int choice;
     char again;
     float rating;
     string comment;
          }
-cout << "Which linked list Method should we use?\n";
-cout << [1] Head << endl;
-cout <<
+    cout << "Which linked list Method should we use?\n";
+    cout << "    [1] New nodes are added at the head of linked list\n";
+    cout << "    [2] New nodes are added at the tail of linked list\n";
+    cout << "    Choice: ";
+    cin >> choice;
+    cin.ignore();
+
+    do {
+      cout << "Enter review rating 0-5: ";
+      cin >> rating; 
+      cin.ignore();
+        "
+      
+    }
