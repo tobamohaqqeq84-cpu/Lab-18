e1306d02b55ffc5f4b5e6bbed4c9c298d7560bc6
tobam <tobamohaqqeq84@gmail.com>
2025-10-06 00:46:49 +0000

    2nd

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+//Toba Mohaqqeq 
+//COMSC-210
+//lAB-18
+
 #include <iostream>
+#include <string>
+using namespace std;
+
+    struct Node {
+      float rating;
+      string comment;
+      Node next;
+                };
+
+
+    }
+
